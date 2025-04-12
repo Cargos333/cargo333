64a8c0eb342a5d1a88fec9f53040ca6e23817068
tp06559 <tp065995@mail.apu.edu.my>
2025-04-12 09:45:31 +0300

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # cargo333
 # cargo333
 # cargo333
+# cargo333
--- db_config.py
+++ db_config.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
     'DATABASE_URL',
-    'postgresql://cargo_db_ofg3_user:OlxdBEGckM3DnLYLePaZunBnhmrEdKpa@dpg-cvt0ed24d50c73db1n8g-a/cargo_db_ofg3'
+    'postgresql://cargo_db_ofg3_user:OlxdBEGckM3DnLYLePaZunBnhmrEdKpa@dpg-cvt0ed24d50c73db1n8g-a.oregon-postgres.render.com/cargo_db_ofg3'
 )
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'a7153e2ffe196b572ee474300a7fd13b')
 db = SQLAlchemy(app)
